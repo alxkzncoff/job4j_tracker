5aeae1956f4614d2af627fc2d36ab6ae3b3a2f4c
alxkzcoff <alx.kzncoff@gmail.com>
2021-09-11 13:35:30 +0300

    5.1. Зона видимости переменных [#180147 #201812]

--- src/main/java/ru/job4j/max/Reduce.java
+++ src/main/java/ru/job4j/max/Reduce.java
@@ -3,8 +3,8 @@ package ru.job4j.max;
 public class Reduce {
     private int[] array;
 
-    public void to(int[] data) {
-        this.array = data;
+    public void to(int[] array) {
+        this.array = array;
     }
 
     public void print() {
