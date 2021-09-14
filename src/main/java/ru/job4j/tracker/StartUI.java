2dd4a327c8cdb89965009023f711e7970c1940ce
alxkzcoff <alx.kzncoff@gmail.com>
2021-09-14 13:16:50 +0300

    9. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -6,9 +6,6 @@ import java.time.format.DateTimeFormatter;
 public class StartUI {
     public static void main(String[] args) {
         Item item = new Item();
-        LocalDateTime currentDateTime = item.getDateTime();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        String currentDateTimeFormat = currentDateTime.format(formatter);
-        System.out.println(currentDateTimeFormat);
+        System.out.println(item);
     }
 }
