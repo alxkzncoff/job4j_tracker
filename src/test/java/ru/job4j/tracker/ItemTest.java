0217d509baf0b645e1ef310053ceabf1242e1b8e
Aleksandr <alx.kzncoff@gmail.com>
2021-10-03 13:36:16 +0300

    0. Сортировка [#10096 #211004]

--- src/test/java/ru/job4j/tracker/ItemTest.java
+++ src/test/java/ru/job4j/tracker/ItemTest.java
@@ -14,16 +14,16 @@ public class ItemTest {
     @Test
     public void itemSortAsc() {
         List<Item> items = Arrays.asList(
-                new Item("", 4),
-                new Item("", 2),
-                new Item("", 3),
-                new Item("", 1)
+                new Item("Bug", 4),
+                new Item("Fix", 2),
+                new Item("Issue", 3),
+                new Item("Patch", 1)
         );
         List<Item> expected = Arrays.asList(
-                new Item("", 1),
-                new Item("", 2),
-                new Item("", 3),
-                new Item("", 4));
+                new Item("Patch", 1),
+                new Item("Fix", 2),
+                new Item("Issue", 3),
+                new Item("Bug", 4));
         Collections.sort(items);
         assertEquals(expected, items);
     }
@@ -31,16 +31,16 @@ public class ItemTest {
     @Test
     public void itemSortDesc() {
         List<Item> items = Arrays.asList(
-                new Item("", 2),
-                new Item("", 1),
-                new Item("", 3),
-                new Item("", 4)
+                new Item("Fix", 2),
+                new Item("Patch", 1),
+                new Item("Issue", 3),
+                new Item("Bug", 4)
         );
         List<Item> expected = Arrays.asList(
-                new Item("", 4),
-                new Item("", 3),
-                new Item("", 2),
-                new Item("", 1));
+                new Item("Bug", 4),
+                new Item("Issue", 3),
+                new Item("Fix", 2),
+                new Item("Patch", 1));
         Collections.sort(items, new SortItemDesc());
         assertEquals(expected, items);
     }
