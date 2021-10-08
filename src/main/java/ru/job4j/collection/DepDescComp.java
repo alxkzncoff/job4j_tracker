f6bc2c7430d449083952a30254d29481d57f9800
alxkzcoff <alx.kzncoff@gmail.com>
2021-10-08 09:56:51 +0300

    2. Отсортировать департаменты [#34352 #213313]

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -2,12 +2,16 @@ package ru.job4j.collection;
 
 import java.util.Comparator;
 
+/**
+ * Компаратор сравнивает первые элементы по убыванию,
+ * если они равны, то сравнивает последующие элементы,
+ * но в возрастающем порядке.
+ * @author Aleksandr Kuznetsov
+ * @version 1.0
+ */
 public class DepDescComp implements Comparator<String> {
     public int compare(String o1, String o2) {
         int result = o2.split("/")[0].compareTo(o1.split("/")[0]);
-        if (result == 0) {
-            return o1.compareTo(o2);
-        }
-        return result;
+        return result == 0 ? o1.compareTo(o2) : result;
     }
 }
