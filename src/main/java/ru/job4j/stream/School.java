06964cae6b36943b7aa9c47090aeccf395236825
alxkzcoff <alx.kzncoff@gmail.com>
2021-10-14 11:12:56 +0300

    1. Фильтрация учеников [#110058]

--- src/main/java/ru/job4j/stream/School.java
+++ src/main/java/ru/job4j/stream/School.java
@@ -4,7 +4,21 @@ import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
+/**
+ *  В школе требование разделить класс на три класса по общему баллу.
+ *  [70; 100]
+ *  [50; 70)
+ *  (0; 50)
+ * @author Aleksandr Kuznetsov
+ * @version 1.0
+ */
 public class School {
+    /**
+     * Фильтрует учеников по заданным критериям.
+     * @param students список учеников.
+     * @param predict фильтр.
+     * @return список отфильтрованных учеников.
+     */
     public List<Student> collect(List<Student> students, Predicate<Student> predict) {
         return students.stream()
                 .filter(predict)
