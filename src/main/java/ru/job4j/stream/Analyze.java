f441cbbef0a325009d78822275661824b5281237
alxkzcoff <alx.kzncoff@gmail.com>
2021-10-21 10:41:59 +0300

    2. Аттестация [#49777 #218504]

--- src/main/java/ru/job4j/stream/Analyze.java
+++ src/main/java/ru/job4j/stream/Analyze.java
@@ -1,6 +1,7 @@
 package ru.job4j.stream;
 
 import java.util.Comparator;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -33,7 +34,8 @@ public class Analyze {
         return stream.map(pupil -> new Tuple(pupil.getName(),
                 pupil.getSubjects().stream()
                         .mapToInt(Subject::getScore)
-                                .average().orElse(0D)))
+                                .average()
+                        .orElse(0D)))
                 .collect(Collectors.toList());
     }
 
@@ -44,7 +46,9 @@ public class Analyze {
      */
     public static List<Tuple> averageScoreByPupil(Stream<Pupil> stream) {
         return stream.flatMap(subject -> subject.getSubjects().stream())
-                .collect(Collectors.groupingBy(Subject::getName,
+                .collect(Collectors.groupingBy(
+                        Subject::getName,
+                        LinkedHashMap::new,
                         Collectors.averagingDouble(Subject::getScore)))
                 .entrySet().stream()
                 .map(subject -> new Tuple(subject.getKey(), subject.getValue()))
@@ -61,7 +65,8 @@ public class Analyze {
         return stream.map(pupil -> new Tuple(pupil.getName(),
                         pupil.getSubjects().stream()
                                 .mapToInt(Subject::getScore)
-                                .sum())).max(Comparator.comparingDouble(Tuple::getScore))
+                                .sum()))
+                .max(Comparator.comparingDouble(Tuple::getScore))
                 .orElse(null);
     }
 
