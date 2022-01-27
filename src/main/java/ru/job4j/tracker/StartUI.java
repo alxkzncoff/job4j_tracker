c4a57b86f2df8417691d246fe6f36a18a9801b23
alxkzcoff <alx.kzncoff@gmail.com>
2022-01-27 16:18:30 +0300

    1. Эксперименты с различными GC [#1590]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -22,9 +22,29 @@ public class StartUI {
     }
 
     public static void main(String[] args) {
-        Input validate = new ValidateInput(
-                new ConsoleInput()
-        );
+        Input in = new StubInput(new String[] {
+                "0", "test1",
+                "0", "test2",
+                "0", "test3",
+                "0", "test4",
+                "0", "test5",
+                "0", "test6",
+                "0", "test7",
+                "0", "test8",
+                "0", "test9",
+                "0", "test10",
+                "3",
+                "1", "1", "test1 replaced",
+                "1", "2", "test2 replaced",
+                "1", "3", "test3 replaced",
+                "3",
+                "2", "1",
+                "2", "2",
+                "2", "3",
+                "3",
+                "4", "4",
+                "5", "test5",
+                "6"});
         Output output = new ConsoleOutput();
         List<UserAction> actions = List.of(
                 new CreateAction(output),
@@ -37,6 +57,6 @@ public class StartUI {
         );
         SqlTracker tracker = new SqlTracker();
         tracker.init();
-        new StartUI().init(validate, tracker, actions);
+        new StartUI().init(in, tracker, actions);
     }
 }
