15b4229bef39ab3450c340626058e3e884228ff4
alxkzcoff <alx.kzncoff@gmail.com>
2022-05-06 15:32:08 +0300

    1. HbmTracker. [#287705]

---
+++ src/main/java/ru/job4j/tracker/HbmTracker.java
@@ -0,0 +1,83 @@
+package ru.job4j.tracker;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import java.util.List;
+
+public class HbmTracker implements Store, AutoCloseable {
+    private final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+            .configure().build();
+    private final SessionFactory sf = new MetadataSources(registry)
+            .buildMetadata().buildSessionFactory();
+
+    @Override
+    public Item add(Item item) {
+        Session session = sf.openSession();
+        session.beginTransaction();
+        session.save(item);
+        session.getTransaction().commit();
+        session.close();
+        return null;
+    }
+
+    @Override
+    public boolean replace(int id, Item item) {
+        Session session = sf.openSession();
+        session.beginTransaction();
+        session.update(String.valueOf(id), item);
+        session.getTransaction().commit();
+        session.close();
+        return true;
+    }
+
+    @Override
+    public boolean delete(int id) {
+        Session session = sf.openSession();
+        session.beginTransaction();
+        Item item = new Item(null);
+        item.setId(id);
+        session.delete(item);
+        session.getTransaction().commit();
+        session.close();
+        return true;
+    }
+
+    @Override
+    public List<Item> findAll() {
+        Session session = sf.openSession();
+        session.beginTransaction();
+        List result = session.createQuery("from ru.job4j.tracker.Item").list();
+        session.getTransaction().commit();
+        session.close();
+        return result;
+    }
+
+    @Override
+    public List<Item> findByName(String key) {
+        Session session = sf.openSession();
+        session.beginTransaction();
+        List result = session.createQuery("from ru.job4j.tracker.Item where name = :key").list();
+        session.getTransaction().commit();
+        session.close();
+        return result;
+    }
+
+    @Override
+    public Item findById(int id) {
+        Session session = sf.openSession();
+        session.beginTransaction();
+        Item result = session.get(Item.class, id);
+        session.getTransaction().commit();
+        session.close();
+        return result;
+    }
+
+    @Override
+    public void close() throws Exception {
+        StandardServiceRegistryBuilder.destroy(registry);
+    }
+}
